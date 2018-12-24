e2f5f773c41d719e7e396d3bcb54c2f4a3db7db6
crummers <marc.carruthers@hotmail.com>
2018-12-24 15:58:46 +0000

    Added jQuery functionality to navigation

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-let mainNav = $('#js-menu');
-let navBarToggle = $('#js-navbar-toggle');
+$(document).ready(() => {
+  $('#js-navbar-toggle').on('mouseenter', () => {
+    $('#js-menu').addClass('active').on('mouseleave', () => {
+      $('#js-menu').removeClass('active');
+    });
+  });
 
-navBarToggle.addEventListener('click', function() {
-    mainNav.classList.toggle('.active');
-
-    if (mainNav.className == '.main-nav active') {
-      mainNav.addEventListener('click', function() {
-        mainNav.classList.remove('.active');
-      });
-    }
-});
-
-$(function () {
-
-        $('Send Message').on('click', function (event) {
-
-          event.preventDefault();
-
-          $.ajax({
-            type: 'post',
-            url: 'post_form.php',
-            data: $('form').serialize(),
-            success: function () {
-              alert('form was submitted');
-            }
-          });
-
-        });
-
-      });
+  $('#js-menu').on('click', () => {
+    $('#js-menu').removeClass('active')
+  });
+})
