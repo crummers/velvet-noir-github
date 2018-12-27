a21bc01f6de1adf2c2dbc255fa6ac2c7e2e480d9
crummers <marc.carruthers@hotmail.com>
2018-12-27 10:59:00 +0000

    posts form in background. resized bg image and fixed bg on mobile

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 $(document).ready(() => {
-  $('#js-navbar-toggle').on('mouseenter', () => {
-    $('#js-menu').addClass('active').on('mouseleave', () => {
-      $('#js-menu').removeClass('active');
-    });
+  $('#js-navbar-toggle').on('click', () => {
+    $('#js-menu').toggleClass('active')
   });
 
   $('#js-menu').on('click', () => {
     $('#js-menu').removeClass('active')
   });
+
+  $( "form" ).on( "submit", function( event ) {
+  event.preventDefault();
+  $.get('post_form.php' + $('form').serialize());
+  $.post('post_form.php', $('form').serialize());
+});
 })
